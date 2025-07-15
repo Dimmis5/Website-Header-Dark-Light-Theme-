c2ad399a090a678095c3ee4fd3695b01acf1b64f
Dimmis5 <zhangdimeo@gmail.com>
2025-07-15 16:54:10 +0200

    Fin du projet

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import { Hero } from './components/Hero'
 
 export const App = () => {
 
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem('theme')
+
+    if(savedTheme) {
+
+      return savedTheme === 'dark'
+    } else {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+    }
+
+  })
 
   useEffect(() => {
     if(darkMode) {
       document.documentElement.classList.add('dark')
+      localStorage.setItem('theme', 'dark')
     } else {
       document.documentElement.classList.remove('dark')
+      localStorage.setItem('theme', 'light')
     }
   },[darkMode])
 
